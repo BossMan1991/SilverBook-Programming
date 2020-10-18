c3ac61a3512517300bc48834000bf05d994d77d7
BossMan1991 <72774769+BossMan1991@users.noreply.github.com>
2020-10-17 19:30:22 -0600

    Add files via upload

---
+++ cppProg1.cpp
@@ -0,0 +1,24 @@
+//Libraries
+#include <stdio.h>
+#include <iostream>
+
+//Name Space.
+using namespace std;
+
+//Global Variables.
+char name [20];
+int tallyG, tallyS;
+bool feme, tits, animeSub;
+bool penis, frtnt, tiktok;
+
+int main (void){
+
+    cout << "Please enter your name and press enter." << endl;
+    cin >> name;
+    cout << "Okay " << name << ", this program will evaluate how gay you are. \n"
+         << "Just answer the following questions honestly. \n";
+    
+    
+
+return 0;
+}
---
+++ csharpprog1.cs
@@ -0,0 +1,16 @@
+using system;
+
+namespace HelloWorld
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int nigger = 5;
+            if (nigger <= 5){
+                Console.WriteLine("Hello World");
+            }
+            
+        }
+    }
+}
